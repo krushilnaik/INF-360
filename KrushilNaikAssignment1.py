bd965dd8006e5992f0ccd552b4b397d53bd5d0ad
Krushil Naik <krushilnaik96@gmail.com>
2022-01-18 15:49:05 -0500

    Make output prettier (kinda)

--- KrushilNaikAssignment1.py
+++ KrushilNaikAssignment1.py
@@ -2,8 +2,9 @@
 # Krushil Naik
 # Assignment 1
 
-print("-" * 10 + "Welcome, User!" + "-" * 10)
+print("-" * 10 + " Welcome, User! " + "-" * 10)
 print()
 myInput = input("Type something and press enter: ")
 print()
 print("You typed: " + myInput)
+print("-" * 36)
