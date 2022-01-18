56fca5c244b1e1071b9bee31f69f4a826dfb4493
Krushil Naik <krushilnaik96@gmail.com>
2022-01-18 11:19:17 -0500

    Finish homework 1

---
+++ KrushilNaikAssignment1.py
@@ -0,0 +1,9 @@
+# INF360 - Programming in Python
+# Krushil Naik
+# Assignment 1
+
+print("-" * 10 + "Welcome, User!" + "-" * 10)
+print()
+myInput = input("Type something and press enter: ")
+print()
+print("You typed: " + myInput)
