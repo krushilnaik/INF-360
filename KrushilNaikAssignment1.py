0e574f89632dfc4dead7d6958d1622ad305dd779
Krushil <krushilnaik96@gmail.com>
2022-01-22 19:45:24 +0000

    Containerize repository

--- KrushilNaikAssignment1.py
+++ KrushilNaikAssignment1.py
@@ -7,4 +7,12 @@ print()
 myInput = input("Type something and press enter: ")
 print()
 print("You typed: " + myInput)
+
+length = len(myInput)
+
+print(f"That has {length} character(s).")
+print(f"One more and you would've typed {length + 1}!")
+print(f"Or one less for {length - 1} character(s).")
+print()
+print("Until next time ~Captain Obvious")
 print("-" * 36)
