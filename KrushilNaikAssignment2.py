ec494158ad423ff6fdba128b2fc2bbf4f07bc438
Krushil Naik <krushilnaik96@gmail.com>
2022-01-27 00:35:08 +0000

    Assignment 2

---
+++ KrushilNaikAssignment2.py
@@ -0,0 +1,49 @@
+# INF360 - Programming in Python
+# Krushil Naik
+# Assignment 2
+
+import random
+
+PLAY = True
+print("-" * 10 + " Welcome, User! " + "-" * 10)
+print()
+
+while PLAY:
+    age = int(input("Enter an age (type '0' to quit): "))
+    print()
+
+    if age == 0:
+        break
+
+    if age < 0 or age >= 100:
+        print("You're lying, aren't you? Try again.")
+        continue
+    elif age >= 50:
+        print("You're at least half a century old!")
+    elif age >= 25:
+        print("You're at least a quarter century old!")
+    else:
+        print("This game is rated 25+")
+        break
+
+    randomAge = random.randint(0, age)
+
+    MESSAGE = f"Wow, you're {age - randomAge} years older than someone who's {randomAge}!\n"
+
+    print(MESSAGE)
+
+    print("Now predicting your last 5 Christmas presents:")
+
+    PRESENTS = [
+        "Socks",
+        "Gift card",
+        "Coal",
+        "Sweater",
+        "Taco Bell"
+    ]
+
+    if age > 5:
+        for i in range(5, 0, -1):
+            print(f"{i}) {PRESENTS[i-1]}")
+
+    print()
